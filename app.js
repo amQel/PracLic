49dc037e8dffb3425a4b7cf4edc0fdbfa1a18787
mladosz <milena_ladosz@wp.pl>
2015-06-22 20:51:05 +0200

    added some routing example

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var app = express();
 
+var $ = require('jquery');
+windom.jQuery = $;
 
 //var errorHandler = require('errorhandler');
 
 var routes = require('./routes');
 var path = require('path');
+var static = require('serve-static');
 
 var port = process.env.PORT || 3000;
 
 
 app.use(express.static(path.join(__dirname)));
+
+app.use('/js/jquery.min.js', static(__dirname + '/bower_components/jquery/dist/jquery.min.js'));
+app.use('/js/jquery.min.map', static(__dirname + '/bower_components/jquery/dist/jquery.min.map'));
+
+app.get('/nauczyciel', routes.nauczyciel);
+
 
 
 app.listen(port, function () {
