403fd6e6b5bc154c0f2e007b52e5a34f3e6b03c2
amQel <zoybel7@wp.pl>
2015-06-24 19:37:23 +0200

    szpakowi się jebie

--- routes/index.js
+++ routes/index.js
@@ -39,10 +39,6 @@ module.exports = function(app, passport) {
        });
     });
     
-    app.get('/student', function (req, res) {
-        res.render('student.ejs');
-    });
-    
     app.post('/login', passport.authenticate('login', {
         successRedirect : '/profile',
         failureRedirect : '/login',
